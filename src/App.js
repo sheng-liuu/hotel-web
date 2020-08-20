32e6c86ec341fcc4e53071d37a7ccc6ef02553da
Sheng_Liu <46925731+sheng-liuu@users.noreply.github.com>
2020-08-20 22:51:43 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from './Pages/Home'
+import Rooms from './Pages/Rooms'
+import SingleRoom from './Pages/SingleRoom'
+import Error from './Pages/Error'
+import Navbar from './Components/Navbar'
+import './App.css'
+
+import {Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Switch> {/* to avoid print Error page at each page */}
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        {/* /rooms/+anything can go to singleroom page */}
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+      
     </div>
   );
 }
---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from '../Components/Hero'
+import Banner from '../Components/Banner'
+import {Link} from 'react-router-dom'
+
+export default function Home() {
+  return (
+    <Hero>
+      <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+        <Link to='/rooms' className="btn-primary">our rooms</Link>
+      </Banner>
+    </Hero>
+  );
+}
+

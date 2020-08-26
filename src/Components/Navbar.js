caa5e511aecce33a39faad96402cfb92fb052368
Sheng_Liu <46925731+sheng-liuu@users.noreply.github.com>
2020-08-26 22:43:28 +0200

    finished

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -27,6 +27,7 @@ export default class Navbar extends Component {
                       <FaAlignRight className="nav-icon" />
                   </button>  
               </div>
+              {/** */}
               <ul className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}>
                   <li>
                       <Link to="/">Home</Link>
